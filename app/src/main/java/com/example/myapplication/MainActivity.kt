701e140343727cc12076c8a3c35f4e2dc3531a91
JavMig9903 <javiermiguel9903@gmail.com>
2020-09-30 14:27:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.v(MainActivity::class.java.name,"Este es un Log v")
+        Log.d(MainActivity::class.java.name,"Este es un Log d de debugger")
+        Log.i(MainActivity::class.java.name,"Este es un Log i de información")
+        Log.w(MainActivity::class.java.name,"Este es un Log w de warning")
+        Log.e(MainActivity::class.java.name,"Este es un Log e de error")
+        
+    }
+}
